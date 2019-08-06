3cb316640cda2ab6be211b78be893c4a959e8703
gpc <15927299723@163.com>
2019-08-07 07:37:03 +0800

    add some codes

--- 2gate.py
+++ 2gate.py
@@ -1,4 +1,4 @@
-#！/usr/bin/python3
+# ！/usr/bin/python3
 # -*- coding: utf-8 -*-
 # @ Time    :  0:55
 # @ Author  : Chenny
@@ -6,24 +6,31 @@
 # @ File    : 2gate.py
 # @ Software: PyCharm
 
-# 利用短时能量，短时过零率，使用双门限法进行端点检测
-def endPointDetect(wave_data, energy, zeroCrossingRate) :
+
+def endPointDetect(wave_data, energy, zeroCrossingRate):
+    """
+    # 利用短时能量，短时过零率，使用双门限法进行端点检测
+    :param wave_data:
+    :param energy:
+    :param zeroCrossingRate:
+    :return:
+    """
     sum = 0
     energyAverage = 0
-    for en in energy :
+    for en in energy:
         sum = sum + en
     energyAverage = sum / len(energy)
 
     sum = 0
-    for en in energy[:5] :
+    for en in energy[:5]:
         sum = sum + en
     ML = sum / 5
-    MH = energyAverage / 4              #较高的能量阈值
-    ML = (ML + MH) / 4    #较低的能量阈值
+    MH = energyAverage / 4  # 较高的能量阈值
+    ML = (ML + MH) / 4  # 较低的能量阈值
     sum = 0
-    for zcr in zeroCrossingRate[:5] :
+    for zcr in zeroCrossingRate[:5]:
         sum = float(sum) + zcr
-    Zs = sum / 5                     #过零率阈值
+    Zs = sum / 5  # 过零率阈值
 
     A = []
     B = []
@@ -32,7 +39,7 @@ def endPointDetect(wave_data, energy, zeroCrossingRate) :
     # 首先利用较大能量阈值 MH 进行初步检测
     flag = 0
     for i in range(len(energy)):
-        if len(A) == 0 and flag == 0 and energy[i] > MH :
+        if len(A) == 0 and flag == 0 and energy[i] > MH:
             A.append(i)
             flag = 1
         elif flag == 0 and energy[i] > MH and i - 21 > A[len(A) - 1]:
@@ -42,33 +49,33 @@ def endPointDetect(wave_data, energy, zeroCrossingRate) :
             A = A[:len(A) - 1]
             flag = 1
 
-        if flag == 1 and energy[i] < MH :
+        if flag == 1 and energy[i] < MH:
             A.append(i)
             flag = 0
     print("较高能量阈值，计算后的浊音A:" + str(A))
 
     # 利用较小能量阈值 ML 进行第二步能量检测
-    for j in range(len(A)) :
+    for j in range(len(A)):
         i = A[j]
-        if j % 2 == 1 :
-            while i < len(energy) and energy[i] > ML :
+        if j % 2 == 1:
+            while i < len(energy) and energy[i] > ML:
                 i = i + 1
             B.append(i)
-        else :
-            while i > 0 and energy[i] > ML :
+        else:
+            while i > 0 and energy[i] > ML:
                 i = i - 1
             B.append(i)
     print("较低能量阈值，增加一段语言B:" + str(B))
 
     # 利用过零率进行最后一步检测
-    for j in range(len(B)) :
+    for j in range(len(B)):
         i = B[j]
-        if j % 2 == 1 :
-            while i < len(zeroCrossingRate) and zeroCrossingRate[i] >= 3 * Zs :
+        if j % 2 == 1:
+            while i < len(zeroCrossingRate) and zeroCrossingRate[i] >= 3 * Zs:
                 i = i + 1
             C.append(i)
-        else :
-            while i > 0 and zeroCrossingRate[i] >= 3 * Zs :
+        else:
+            while i > 0 and zeroCrossingRate[i] >= 3 * Zs:
                 i = i - 1
             C.append(i)
     print("过零率阈值，最终语音分段C:" + str(C))
